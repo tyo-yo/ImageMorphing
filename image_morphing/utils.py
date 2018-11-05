0047665657efbfee983c5daeb91752ed6e09aedc
tyo-yo <youtyou0213@gmail.com>
2018-11-05 15:27:04 +0900

    Add utils

--- image_morphing/utils.py
+++ image_morphing/utils.py
@@ -10,10 +10,29 @@ def imshow(img):
 
 def get_color(img, p):
     p = np.array(p)
-    p[0] = np.clip(p[0], 0, img.shape[0] - 1)
-    p[1] = np.clip(p[1], 0, img.shape[1] - 1)
-    p = p.astype(np.int)
-    return img[p[0], p[1]]
+    if p.ndim == 1:
+        p[0] = np.clip(p[0], 0, img.shape[0] - 1)
+        p[1] = np.clip(p[1], 0, img.shape[1] - 1)
+        p = p.astype(np.int)
+        return img[p[0], p[1]]
+    elif p.ndim == 2:
+        p[:, 0] = np.clip(p[:, 0], 0, img.shape[0] - 1)
+        p[:, 1] = np.clip(p[:, 1], 0, img.shape[1] - 1)
+        p = p.astype(np.int)
+        return img[p[:, 0], p[:, 1]]
+    elif p.ndim == 3:
+        p[:, :, 0] = np.clip(p[:, :, 0], 0, img.shape[0] - 1)
+        p[:, :, 1] = np.clip(p[:, :, 1], 0, img.shape[1] - 1)
+        p = p.astype(np.int)
+        return img[p[:, :, 0], p[:, :, 1]]
+    elif p.ndim == 4:
+        p[:, :, :, 0] = np.clip(p[:, :, :, 0], 0, img.shape[0] - 1)
+        p[:, :, :, 1] = np.clip(p[:, :, :, 1], 0, img.shape[1] - 1)
+        p = p.astype(np.int)
+        return img[p[:, :, :, 0], p[:, :, :, 1]]
+
+def get_v(v, p):
+    return get_color(v, p)
 
 def save_animation(imgs, file_name='animation.mov', time=1):
     fps = len(imgs) // time
@@ -25,3 +44,53 @@ def save_animation(imgs, file_name='animation.mov', time=1):
         imgs[0].shape[:2])
     for img in imgs:
         out.write(img)
+
+def describe(x):
+    print('*** Numpy array info ***')
+    print('shape : ' + str(x.shape))
+    print('mean : {:.4f}'.format(x.mean()))
+    print('std : {:.4f}'.format(x.std()))
+    print('min : {:.4f}, argmin : {}'.format(
+        x.min(),
+        str(np.unravel_index(x.argmin(), x.shape))))
+    print('max : {:.4f}, argmax : {}'.format(
+        x.max(),
+        str(np.unravel_index(x.argmax(), x.shape))))
+
+def load_points(path):
+    with open(path, 'r', encoding='utf-8') as f:
+        s = f.read()
+        arr = s.splitlines()
+        points = [p.split(', ') for p in arr]
+        return np.array(points).astype(np.int)
+
+def resize_all(size, img0, img1, v, p0, p1):
+    img0_low = cv2.resize(img0, (size, size))
+    img1_low = cv2.resize(img1, (size, size))
+
+    vx = v[:, :, 0]
+    vy = v[:, :, 1]
+    vx_low = cv2.resize(vx, (size, size))
+    vy_low = cv2.resize(vy, (size, size))
+    vx_low = vx_low[:, :, np.newaxis]
+    vy_low = vy_low[:, :, np.newaxis]
+    v_low = np.concatenate([vx_low, vy_low], axis=2)
+    v_low = v_low / img0.shape[0] * size
+
+    p0_low = p0 / img0.shape[0] * size
+    p1_low = p1 / img1.shape[0] * size
+
+    return img0_low, img1_low, v_low, p0_low, p1_low
+
+
+def plot_correspondances(img0, img1, p0, p1):
+    img0_point = img0 // 3
+    img1_point = img1 // 3
+    img0_point[p0[:,0].astype(int) ,p0[:,1].astype(int), 0:2] = 255
+    img1_point[p1[:,0].astype(int) ,p1[:,1].astype(int), 0:2] = 255
+    plt.figure(figsize=(10, 5))
+    plt.subplot(1, 2, 1)
+    plt.imshow(img0_point[:, :, ::-1])
+    plt.subplot(1, 2, 2)
+    plt.imshow(img1_point[:, :, ::-1])
+    plt.show()
